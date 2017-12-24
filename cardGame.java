913389472ce6c33f1ff2af7a93b8cd8fb379809e
Ursu Razvan <razvan.ursu@ymail.com>
2017-12-24 20:44:33 +0200

    More small steps

---
+++ cardGame.java
@@ -0,0 +1,10 @@
+
+public class cardGame {
+
+	public static void main(String[] args) {
+		System.out.println("Even more small steps...");
+		System.out.println("I need to do actual stuff tomorrow");
+
+	}
+
+}
