d73dd35c755c0b2e84aa418be30e3622d8ca6880
Ursu Razvan <razvan.ursu@ymail.com>
2017-12-25 10:03:20 +0200

    Some code + new dirrection

--- cardGame.java
+++ cardGame.java
@@ -2,9 +2,25 @@
 public class cardGame {
 
 	public static void main(String[] args) {
-		System.out.println("Even more small steps...");
-		System.out.println("I need to do actual stuff tomorrow");
-
+		
+		cardSet cardPack = new cardSet();
+		cardPack.makePack();
 	}
 
+}
+
+ class cardSet {
+	String[] set = {"Ace", "2", "3","4","5","6","7","8","9","10","Jack","Queen","King"};
+	String[] suite = {"Diamonds","Spades","Hearts","Clubs"};
+	
+	String[] pack = new String[52];
+	
+	void makePack () {
+		for(int i=0; i<4; i++) {
+			for(int x=0; x<13; x++) {
+				pack[x]=(set[x]+" of "+suite[i]);
+				System.out.println(pack[x]);
+			}
+		}
+	}
 }
